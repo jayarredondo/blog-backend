974f2806eb40c39ae6c9a9fdce788fd13e278fea
Jay Arredondo <jearredondo13@gmail.com>
2022-12-04 21:11:56 -0600

    fixed seeder

--- src/sql/seeder.sql
+++ src/sql/seeder.sql
@@ -1,8 +1,8 @@
 USE vueblog_db;
 
 # password: groovy
-INSERT INTO users (id, email, password, username)
-VALUES(1, 'jay@email.com', '$2a$10$yOkrbY0g4bNupMXjP8qSG.aygWa8n4hyLJ680yufr7Is4sjMyWnFi', 'jayman123');
+INSERT INTO users (id, email, password, role, username)
+VALUES(1, 'jay@email.com', '$2a$10$yOkrbY0g4bNupMXjP8qSG.aygWa8n4hyLJ680yufr7Is4sjMyWnFi', 'USER', 'jayman123');
 
 INSERT INTO posts (id, body, title, author_id)
 VALUES(1, 'This is my first blog!', 'My First Blog', 1),
